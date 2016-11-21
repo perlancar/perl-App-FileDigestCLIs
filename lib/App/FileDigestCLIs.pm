d7ec735082daf7fc7f6b24c44a84cca9d0e7d2dc
perlancar (@pc-home) <perlancar@gmail.com>
2016-11-21 22:17:52 +0700

    Initial commit

---
+++ lib/App/FileDigestCLIs.pm
@@ -0,0 +1,16 @@
+package App::FileDigestCLIs;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs for File::Digest
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities:
+
+#INSERT_EXECS_LIST
